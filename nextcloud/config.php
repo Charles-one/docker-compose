d78f658f55e0d3560c1cdc76b8244e5b8cd3c4a1
Charles <shaoxin5345@icloud.com>
2019-05-15 01:13:17 +0800

    init

---
+++ nextcloud/config.php
@@ -0,0 +1,39 @@
+<?php
+$CONFIG = array (
+  'htaccess.RewriteBase' => '/',
+  'memcache.local' => '\\OC\\Memcache\\APCu',
+  'apps_paths' => 
+  array (
+    0 => 
+    array (
+      'path' => '/var/www/html/apps',
+      'url' => '/apps',
+      'writable' => false,
+    ),
+    1 => 
+    array (
+      'path' => '/var/www/html/custom_apps',
+      'url' => '/custom_apps',
+      'writable' => true,
+    ),
+  ),
+  'instanceid' => 'ocuqipw39wk2',
+  'passwordsalt' => 'ycBWO8/NlQpND1P4rbxdMzmia/WYvU',
+  'secret' => 'gpZqrTqGvsg5H9QfnzEnU/5OuZoyqRYrUMGXdmIk0OWOCIRT',
+  'trusted_domains' => 
+  array (
+    0 => '107.172.90.222',
+  ),
+  'datadirectory' => '/var/www/html/data',
+  'dbtype' => 'mysql',
+  'version' => '16.0.0.9',
+  'overwrite.cli.url' => 'http://107.172.90.222',
+  'dbname' => 'nextcloud',
+  'dbhost' => 'mariadb',
+  'dbport' => '',
+  'dbtableprefix' => 'oc_',
+  'mysql.utf8mb4' => true,
+  'dbuser' => 'nextcloud',
+  'dbpassword' => 'nextcloud_weiki_JKL_sdf',
+  'installed' => true,
+);
---
+++ readme.md
@@ -0,0 +1,64 @@
+
+## 存放docker-compose文件库, 随意取用
+
+
+## 目录
+
+### <a href="#first">1.运行条件</a>
+### <a href="#two">2.使用示例</a>
+### <a href="#three">3.docker-compose库存</a>
+
+### <a id="first">1.运行条件:</a>
+
+#### 需要docker和docker-compose. 如没安装, 可使用[脚本安装]()
+
+
+### <a id="two" href="">2.使用示例, 这里以wordpress-nginx-mariadb为例</a>
+
+1. 使用git克隆docker-compose库, 并存放在当前用户目录中(即cd ~/ 所在地方)
+
+```
+
+git clone https://github.com/Charles-one/docker-compose.git ~/docker-compose
+
+```
+
+2. 复制一份执行文件到用户目录中
+
+```
+
+cp -r ~/docker-compose/wordpress-nginx-mariadb ~/wordpress-nginx-mariadb
+
+```
+
+3. 进行文件夹中, 使用docker-compose 构建镜
+
+```
+
+# 进行需要运行的docker-compose库中
+cd ~/wordpress-nginx-mariadb
+
+# up 表示构建, -d后台运行
+# 运行后需要下载镜像, 时间比较长.
+docker-compose up -d
+
+```
+
+4. 查看wordpress服务
+
+访问地址:
+
+>http://你的服务器ip
+
+设置用户密码即可使用
+
+5. 更多docker-compose用法请参考这里.
+
+
+### <a id="three" href="">3.docker-compose库存</a>
+
+<a src="wordpress-nginx-mariadb/">wordpress-nginx-mariadb个人博客</a>
+<a src="nextcloud">nextcloud个人私有云</a>
+
+
+
